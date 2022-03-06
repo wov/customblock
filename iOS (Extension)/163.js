4ed6138fedeee2ea6987673393173bf94db038ad
wov <wovfeng@gmail.com>
2022-03-06 22:04:40 +0800

    try to opt the web profamace

--- iOS (Extension)/163.js
+++ iOS (Extension)/163.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('click', event => {
     let target = event.target
     console.log(target.tagName)
@@ -6,3 +5,13 @@ document.addEventListener('click', event => {
         window.location.href = target.href;
     }
 })
+
+// 方法终止对所有目标元素可见性变化的观察。
+// https://developer.mozilla.org/zh-CN/docs/Web/API/IntersectionObserver/disconnect
+window.IntersectionObserver = null;
+
+//remove all scroll events...
+setTimeout( _ => {
+    window.onscroll = null;
+    document.body.onscroll = null;
+},2000)
