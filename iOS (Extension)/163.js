ff881d99c266f246f024cab60d17aef3c72e5c2f
wov <wovfeng@gmail.com>
2022-03-08 08:59:43 +0800

    remove blocker and fiexd some style issues

--- iOS (Extension)/163.js
+++ iOS (Extension)/163.js
@@ -1,6 +1,6 @@
 document.addEventListener('click', event => {
-    let target = event.target
-    console.log(target.tagName)
+    let target = event.target;
+//    console.log(target.tagName)
     if(target.classList.contains('js-open-app') && target.href){
         window.location.href = target.href;
     }
