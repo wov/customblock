697e2c689dcdd58260920734285cc609c5a684b2
wov <wovfeng@gmail.com>
2022-03-10 23:12:13 +0800

    fixed bilibili player

--- iOS (Extension)/bilibili.js
+++ iOS (Extension)/bilibili.js
@@ -2,20 +2,39 @@
 //const video = document.querySelector('video')
 //document.location.href = video.src
 
-if( !document.querySelector('.__puddingPlayInOriginDiv') ){
-    const playInOrignButton = document.createElement('div');
-    playInOrignButton.className = '__puddingPlayInOriginDiv';
-    playInOrignButton.innerText = '布丁扩展：在原生播放器中播放';
+//todo: 判断一下路径。。
 
-    playInOrignButton.addEventListener('click', _ => {
-        const video = document.querySelector('video')
-        document.location.href = video.src
-    });
+document.addEventListener('click', event => {
+    let target = event.target;
+//    console.log(target.tagName)
+    if(target.href){
+        window.location.href = target.href;
+    }
+})
 
-    const playerDom = document.querySelector('.m-video-player');
 
-    playerDom.appendChild(playInOrignButton)
+
+
+
+if(window.location.href.indexOf('video') !== -1){
+    if( !document.querySelector('.__puddingPlayInOriginDiv') ){
+        const playInOrignButton = document.createElement('div');
+        playInOrignButton.className = '__puddingPlayInOriginDiv';
+        playInOrignButton.innerText = '布丁扩展：在原生播放器中播放';
+
+        playInOrignButton.addEventListener('click', _ => {
+            const video = document.querySelector('video')
+            document.location.href = video.src
+        });
+
+        document.body.appendChild(playInOrignButton)
+    }
 }
+
+
+
+
+
 
 
 
