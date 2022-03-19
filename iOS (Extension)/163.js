a694664f9e12bc238037a1a9dd6924aa673a8c8c
wov <wovfeng@gmail.com>
2022-03-19 17:24:27 +0800

    add dark mode for 36kr

--- iOS (Extension)/163.js
+++ iOS (Extension)/163.js
@@ -1,21 +1,26 @@
-document.addEventListener('click', event => {
-    let target = event.target;
-//    console.log(target.tagName)
-    if(target.classList.contains('js-open-app') && target.href){
-        window.location.href = target.href;
-    }
-})
-
-//去掉所有“3天前的新闻列表”
-document.addEventListener('scroll', _ => {
-    
-    const _dom_pubtimes = document.querySelectorAll('.pubtime');
-    [].forEach.call(_dom_pubtimes, function(t,t_index) {
-        if(t.textContent === '3天前' ){
-            console.log(t_index)
-            const _article = t.closest('article');
-            _article.parentNode.removeChild( _article );
+function replaceToAppText(){
+    let As = document.querySelectorAll('a');
+    As.forEach(function(_a) {
+        const _to_app = _a.querySelector('.to-app');
+        if(_to_app && _to_app.textContent.indexOf('打开APP') !== -1){
+            _to_app.textContent = '布丁优化'
         }
     });
+}
+
+replaceToAppText();
+
+document.addEventListener('scroll', _ => {
+    replaceToAppText();
+    
+    //去掉所有“3天前的新闻列表”
+    //    const _dom_pubtimes = document.querySelectorAll('.pubtime');
+    //    [].forEach.call(_dom_pubtimes, function(t,t_index) {
+    //        if(t.textContent === '3天前' ){
+    //            console.log(t_index)
+    //            const _article = t.closest('article');
+    //            _article.parentNode.removeChild( _article );
+    //        }
+    //    });
 })
 
