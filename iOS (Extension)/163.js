cbcb865c0ac4acdc916d36e551f6595ddffe3660
wov <wovfeng@gmail.com>
2022-03-22 01:22:29 +0800

    fixed some style issues

--- iOS (Extension)/163.js
+++ iOS (Extension)/163.js
@@ -14,13 +14,13 @@ document.addEventListener('scroll', _ => {
     replaceToAppText();
     
     //去掉所有“3天前的新闻列表”
-    //    const _dom_pubtimes = document.querySelectorAll('.pubtime');
-    //    [].forEach.call(_dom_pubtimes, function(t,t_index) {
-    //        if(t.textContent === '3天前' ){
-    //            console.log(t_index)
-    //            const _article = t.closest('article');
-    //            _article.parentNode.removeChild( _article );
-    //        }
-    //    });
+    const _dom_pubtimes = document.querySelectorAll('.pubtime');
+    [].forEach.call(_dom_pubtimes, function(t,t_index) {
+        if(t.textContent === '3天前' ){
+            console.log(t_index)
+            const _article = t.closest('article');
+            _article.parentNode.removeChild( _article );
+        }
+    });
 })
 
