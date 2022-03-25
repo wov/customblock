6ed2b276e0f4069d5c20e7839f53ea0ecccfbaca
wov <wovfeng@gmail.com>
2022-03-25 21:38:14 +0800

    add style and js for douban movie

---
+++ iOS (Extension)/douban.js
@@ -0,0 +1,7 @@
+// add meta.
+var meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1, user-scalable=no";
+document.getElementsByTagName('head')[0].appendChild(meta);
+
+
