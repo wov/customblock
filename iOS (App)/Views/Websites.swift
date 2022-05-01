d70f056fab6c0cfbe1fcafba9db780c731338a61
wov <wovfeng@gmail.com>
2022-05-01 17:50:08 +0800

    add myseller

--- iOS (App)/Views/Websites.swift
+++ iOS (App)/Views/Websites.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct Websites: View {
-    
     @Environment(\.openURL) var openURL
-    
     
     private func sendMail() {
         let mailtoString = "mailto:wovfeng@gmail.com".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
@@ -20,175 +18,107 @@ struct Websites: View {
         }
     }
     
+    enum types : CaseIterable {
+        case additional
+        case dark
+        case pc
+        case speed
+        
+        var icon: Image {
+            switch self {
+            case .additional:
+                return Image(systemName: "puzzlepiece.extension")
+            case .dark:
+                return Image(systemName: "moon.stars.fill")
+            case .pc:
+                return  Image(systemName: "desktopcomputer")
+            case .speed:
+                return Image(systemName: "speedometer")
+            }
+        }
+        
+        var describe: String {
+            switch self {
+            case .additional:
+                return "针对该网站做过特殊的优化"
+            case .dark:
+                return "跟随系统的暗黑模式"
+            case .pc:
+                return "优化了PC样式的显示"
+            case .speed:
+                return "性能优化"
+            }
+        }
+        
+        var note: String {
+            switch self {
+            case .additional:
+                return "去广告、默认展开、屏蔽等"
+            case .dark:
+                return "系统处于暗黑模式时自动适配"
+            case .pc:
+                return "点击\"大小\">请求桌面网站"
+            case .speed:
+                return "解决浏览器可能会崩溃的问题"
+            }
+        }
+        
+    }
+    
+    struct WebSite{
+        let id =  UUID()
+        var name: String
+        var url: String
+        var types : [types]
+    }
+    
     var body: some View {
+        
+        let websites:[WebSite] = [
+            WebSite(name:"百度",url: "https://baidu.com",types: [.additional,.dark]),
+            WebSite(name:"豆瓣电影",url: "https://movie.douban.com",types: [.pc,.dark]),
+            WebSite(name:"哔哩哔哩",url: "https://m.bilibili.com",types: [.additional,.pc]),
+            WebSite(name:"新浪微博",url: "https://m.weibo.cn",types: [.additional,.dark]),
+            WebSite(name:"网易新闻",url: "https://3g.163.com",types: [.speed,.additional,.dark]),
+            WebSite(name:"腾讯新闻",url: "https://xw.qq.com",types: [.dark]),
+            WebSite(name:"知乎",url: "https://www.zhihu.com",types: [.additional,.dark]),
+            WebSite(name:"36氪",url: "https://m.36kr.com",types: [.additional,.dark]),
+            WebSite(name:"爱范儿",url: "https://www.ifanr.com",types: [.dark]),
+            WebSite(name:"什么值得买",url: "https://smzdm.com",types: [.additional]),
+            WebSite(name:"管家婆",url: "https://passport.wsgjp.com.cn/erp/login",types: [.pc]),
+            WebSite(name:"千牛后台",url: "https://myseller.taobao.com/",types: [.pc])
+        ]
+        
+        
         List{
             Section( header: Text("支持的网站")){
                 Group{
-                    HStack {
-                        Link("百度", destination: URL(string: "https://baidu.com")!)
-                        
-                        Spacer()
+                    ForEach(websites, id: \.id ) { site in
                         HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("豆瓣电影", destination: URL(string: "https://movie.douban.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "desktopcomputer")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("哔哩哔哩", destination: URL(string: "https://m.bilibili.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "desktopcomputer")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("新浪微博", destination: URL(string: "https://m.weibo.cn")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("网易新闻", destination: URL(string: "https://3g.163.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            
-                            Image(systemName: "speedometer")
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("腾讯新闻", destination: URL(string: "https://xw.qq.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("知乎", destination: URL(string: "https://www.zhihu.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("36氪", destination: URL(string: "https://m.36kr.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("爱范儿", destination: URL(string: "https://www.ifanr.com")!)
-                        
-                        Spacer()
-                        HStack {
-                            Image(systemName: "moon.stars.fill")
-                        }
-                    }
-                    
-                    HStack {
-                        Link("什么值得买", destination: URL(string: "https://smzdm.com")!)
-                        Spacer()
-                        HStack {
-                            Image(systemName: "puzzlepiece.extension")
-                        }
-                    }
-                    
-                }
-                
-                Group{
-                    HStack {
-                        Link("管家婆", destination: URL(string: "https://passport.wsgjp.com.cn/erp/login")!)
-                        Spacer()
-                        HStack {
-                            Image(systemName: "desktopcomputer")
+                            Link(site.name, destination: URL(string: site.url)!)
+                            Spacer()
+                            HStack {
+                                ForEach( site.types, id: \.self ){ type in
+                                    type.icon
+                                }
+                            }
                         }
                     }
                 }
             }
             
             Section( header: Text("图标说明")){
-                HStack{
-                    Image(systemName: "puzzlepiece.extension")
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("针对该网站做过特殊的优化")
-                            .font(.footnote)
-                        Text("去广告、默认展开、屏蔽等")
-                            .font(.footnote)
-                            .foregroundColor(Color.gray)
-                    }
-                }
-                
-                HStack{
-                    Image(systemName: "moon.stars.fill")
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("跟随系统的暗黑模式")
-                            .font(.footnote)
-                        Text("系统处于暗黑模式时自动适配")
-                            .font(.footnote)
-                            .foregroundColor(Color.gray)
-                    }
-                }
-                
-                HStack{
-                    Image(systemName: "desktopcomputer")
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("优化了PC样式的显示")
-                            .font(.footnote)
-                        VStack {
-                            HStack {
-                                Text("点击")
-                                Image(systemName:"textformat")
-                                Text("> 请求桌面网站")
-                                Image(systemName:"desktopcomputer")
-                            }
+                ForEach(types.allCases, id: \.self ){ type in
+                    HStack{
+                        type.icon
+                        Spacer()
+                        VStack(alignment: .trailing){
+                            Text( type.describe )
+                                .font(.footnote)
+                            Text( type.note )
+                                .font(.footnote)
+                                .foregroundColor(Color.gray)
                         }
-                        .font(.footnote)
-                        .foregroundColor(Color.gray)
-                        
-                    }
-                }
-                
-                HStack{
-                    Image(systemName: "speedometer")
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("性能优化")
-                            .font(.footnote)
-                        Text("解决浏览器可能会崩溃的问题")
-                            .font(.footnote)
-                            .foregroundColor(Color.gray)
                     }
                 }
             }
---
+++ iOS (Extension)/myseller.js
@@ -0,0 +1,3 @@
+// 添加一个全局的class 用来方便修改样式
+document.querySelector('body').classList.add('pd__ex');
+
