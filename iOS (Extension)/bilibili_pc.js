c96ba2342e7c2c47c8ca7f780f163069f83b9589
wov <wovfeng@gmail.com>
2022-08-20 18:53:11 +0800

    bilibili add block key words

--- iOS (Extension)/bilibili_pc.js
+++ iOS (Extension)/bilibili_pc.js
@@ -1,5 +1,6 @@
 const BS_NAME = "pudding_block_ups";
 const BAD_NAME = "pudding_block_channels";
+const BK_NAME = "pudding_block_keywords";
 
 // add a class for reset the styles easly.
 document.querySelector('body').classList.add('pd__ex');
@@ -62,8 +63,10 @@ function addButtonAndBlock(){
     const CLASSFLAG = "pudding_extension";
     if(!items.length){return;}
     
+    BlockContent();
+    
     items.forEach( i => {
-        if(i.classList.contains(CLASSFLAG)){console.log('it already has..');return;}
+        if(i.classList.contains(CLASSFLAG)){return;}
         i.classList.add(CLASSFLAG);
         // 添加直接展开的功能
 //        const t = i.querySelector('.video-card__content');
@@ -77,12 +80,6 @@ function addButtonAndBlock(){
         if(h && upDom){
             let up = upDom.textContent.replaceAll(/\s+/g, '');
             if(!up){return;}
-            
-            let blockUsers = localStorage.getItem(BS_NAME) ? JSON.parse(localStorage.getItem(BS_NAME)) : [];
-            if(blockUsers.includes(up)){
-                i.remove();
-            }
-            
             const blockDom = document.createElement('div');
             blockDom.className = '__pudding_block';
             h.appendChild(blockDom)
@@ -90,14 +87,40 @@ function addButtonAndBlock(){
             blockDom.addEventListener('click', e => {
                 e.stopPropagation();
                 e.preventDefault();
-                
                 //TODO: 这里改成弹出可添加关键字的弹窗。
+                pd_showBlockModal(up);
+            })
+        }
+    })
+}
+
+function BlockContent(){
+    const items = document.querySelectorAll('.video-card,.rank-item,.bili-video-card,.bili-live-card');
+    items.forEach( i => {
+        
+        // 添加屏蔽的功能
+        // up主的名字..
+        const upDom = i.querySelector('.up-name,.bili-video-card__info--author,.bili-live-card__info--uname');
+        const titleDom = i.querySelector('.bili-video-card__info--tit,.bili-live-card__info--tit,.bili-movie-card__info--tit');
                 
-                const c = confirm(`【布丁扩展】\n\n是否在列表中屏蔽“${up}”？\n屏蔽仅在本地有效`);
-                if(c){
-                    let blockUsers = localStorage.getItem(BS_NAME) ? JSON.parse(localStorage.getItem(BS_NAME)) : [];
-                    blockUsers.push(up);
-                    localStorage.setItem(BS_NAME,JSON.stringify(blockUsers));
+        if(upDom){
+            let up = upDom.textContent.replaceAll(/\s+/g, '');
+            if(!up){return;}
+            
+            let blockUsers = localStorage.getItem(BS_NAME) ? JSON.parse(localStorage.getItem(BS_NAME)) : [];
+            if(blockUsers.includes(up)){
+                i.remove();
+            }
+        }
+        
+        if(titleDom){
+            let title = titleDom.textContent.replaceAll(/\s+/g, '');
+            if(!title){return;}
+            
+            let blockKeyWords = localStorage.getItem(BK_NAME) ? JSON.parse(localStorage.getItem(BK_NAME)) : [];
+            blockKeyWords.forEach( w => {
+                if(title.indexOf(w) !== -1){
+                    console.log('bingo',w);
                     i.remove();
                 }
             })
@@ -106,10 +129,89 @@ function addButtonAndBlock(){
 }
 
 
-function pd_showBlockModal(){
+function pd_showBlockModal(up){
     
+    if(document.querySelector('.pudding_block_Modal')){return;}
     
+    const blockModal = document.createElement('div');
+    blockModal.className = 'pudding_block_Modal';
     
+    const inner = document.createElement('div');
+    inner.className = 'pudding_block_inner';
+    
+    const description = document.createElement('h3');
+    description.textContent = "布丁扩展:屏蔽up主，或者标题关键词。屏蔽只在本地有效。"
+    
+    const upLabel = document.createElement('label');
+    const upCheck = document.createElement('input');
+    upCheck.type = 'checkbox';
+    
+    const upSpan = document.createElement('span');
+    upSpan.textContent = `屏蔽：${up}`;
+    upSpan.checked = true;
+    
+    const keyWordLabel = document.createElement('label');
+    const keySpan = document.createElement('span');
+    keySpan.textContent = '关键词:'
+    const keyInput = document.createElement('input');
+    keyInput.type = "text";
+    keyInput.placeholder = "填入你想屏蔽的关键词";
+    
+    const buttons = document.createElement('div');
+    buttons.className = "buttons";
+    
+    const confirm = document.createElement('button');
+    confirm.textContent = '确定';
+    
+    const cancel = document.createElement('button');
+    cancel.textContent = '取消';
+    
+    inner.appendChild(description);
+    upLabel.appendChild(upCheck)
+    upLabel.appendChild(upSpan)
+    inner.appendChild(upLabel);
+    
+    keyWordLabel.appendChild(keySpan);
+    keyWordLabel.appendChild(keyInput);
+    inner.appendChild(keyWordLabel);
+
+    buttons.appendChild(confirm);
+    buttons.appendChild(cancel);
+    inner.appendChild(buttons);
+    
+    blockModal.appendChild(inner);
+    document.body.appendChild(blockModal);
+    
+    //取消按钮
+    cancel.addEventListener('click',e => {
+        blockModal.remove();
+    });
+    
+    //确定按钮
+    confirm.addEventListener('click',e => {
+        //添加到up主屏蔽名单
+        if(upCheck.checked){
+            let blockUsers = localStorage.getItem(BS_NAME) ? JSON.parse(localStorage.getItem(BS_NAME)) : [];
+            blockUsers.push(up);
+            
+            
+            
+            localStorage.setItem(BS_NAME,JSON.stringify(blockUsers));
+        }
+        //添加到关键字屏蔽名单
+        if(keyInput.value){
+            let blockKeyWords = localStorage.getItem(BK_NAME) ? JSON.parse(localStorage.getItem(BK_NAME)) : [];
+            blockKeyWords.push(keyInput.value);
+            
+            blockKeyWords = blockKeyWords.filter(function(item, pos) {
+                return blockKeyWords.indexOf(item) == pos;
+            })
+            
+            localStorage.setItem(BK_NAME,JSON.stringify(blockKeyWords));
+        }
+        BlockContent();
+        blockModal.remove();
+    });
     
 }
 
@@ -118,7 +220,7 @@ setTimeout(_ => {
 },1000)
 
 
-const rootChangeDom = document.querySelector('#app,main.bili-layout,#i_cecream');
+const rootChangeDom = document.querySelector('#app,main.bili-layout,main.bili-feed4-layout');
 if(rootChangeDom){
     const config = { attributes: false, childList: true, subtree: true };
     const callback = function(mutationsList, observer) {
