527ed6c042163311f560aa57817633edcfb3537d
wov <wovfeng@gmail.com>
2022-10-19 20:16:18 +0800

    add dark mode for zhihu

--- iOS (Extension)/zhihu.js
+++ iOS (Extension)/zhihu.js
@@ -1,10 +1,17 @@
+// 添加一个全局的class 用来方便修改样式
+document.querySelector('html').classList.add('pd__ex');
+
+/***之前使用知乎自己的暗黑模式Start***/
+//
 const rootHtml = document.documentElement;
 const config = { attributes: true, childList: true, subtree: true };
-
+//
 const callback = function(mutationsList, observer) {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        rootHtml.setAttribute('data-theme','dark');
-    }
+//    document.querySelector('body').classList.add('pd__ex');
+//    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//        rootHtml.setAttribute('data-theme','dark');
+//    }
+    removeAllTarget();
 };
 
 const observer = new MutationObserver(callback);
@@ -13,7 +20,10 @@ observer.observe(document.body, config);
 function addDarkMeta(){
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         // remove origin theme color...
-        document.querySelector("[name='theme-color']").remove();
+        
+        if( document.querySelector("[name='theme-color']")){
+            document.querySelector("[name='theme-color']").remove();
+        }
 
         //add a dark meta..
         var meta = document.createElement('meta');
@@ -25,3 +35,21 @@ function addDarkMeta(){
 }
 
 addDarkMeta();
+
+/***之前使用知乎自己的暗黑模式End***/
+
+
+
+// 去掉所有的 a target 属性
+function removeAllTarget(){
+    let As = document.querySelectorAll('a');
+    As.forEach(function(_a) {
+        _a.removeAttribute('target');
+    });
+}
+
+removeAllTarget();
+
+//document.addEventListener('scroll', _ => {
+//    removeAllTarget();
+//})
