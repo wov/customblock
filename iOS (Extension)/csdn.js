d4f919f5a452080e4beac85638269f6672ad7e11
wov <wovfeng@gmail.com>
2022-12-15 21:48:47 +0800

    dark mode for home page

--- iOS (Extension)/csdn.js
+++ iOS (Extension)/csdn.js
@@ -1,6 +1,27 @@
 // 添加一个全局的class 用来方便修改样式
 document.querySelector('body').classList.add('pd__ex');
 
+function addDarkMeta(){
+//    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // remove origin theme color...
+        
+//        if( document.querySelector("[name='theme-color']")){
+//            document.querySelector("[name='theme-color']").remove();
+//        }
+
+        //add a dark meta..
+        var meta = document.createElement('meta');
+        meta.name = "theme-color";
+        meta.content = "#121212";
+        meta.media = "(prefers-color-scheme: dark)";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+//    }
+}
+
+addDarkMeta();
+
+
+
 // 移除target，手机端真的不需要新窗口打开了，啊喂～
 function removeAllTarget(){
     let As = document.querySelectorAll('a');
